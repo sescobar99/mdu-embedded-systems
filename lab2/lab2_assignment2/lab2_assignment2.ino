b421fcb0926bbead86f2101a45feaf5565b8e8f2
sescobar99 <santi.e.c@hotmail.com>
2022-09-28 21:01:55 +0200

    lab2 assigment2 joystick control

--- lab2/lab2_assignment2/lab2_assignment2.ino
+++ lab2/lab2_assignment2/lab2_assignment2.ino
@@ -1,18 +1,14 @@
+/**
+ * MDU ES Lab2 Assignment2
+ * Add another function, which uses the potentiometer values as the basis of input instead of the UART.
+ *  Josep Naranjo
+ *  &
+ *  Santiago Escobar
+ */
+//*****************************************************************************
 // Core library for code-sense
 #include "Energia.h"
-
-// Include application, user and local libraries
 #include <SPI.h>
-
-#include <LCD_screen.h>
-#include <LCD_screen_font.h>
-#include <LCD_utilities.h>
-#include <Screen_HX8353E.h>
-#include <Terminal12e.h>
-#include <Terminal6e.h>
-#include <Terminal8e.h>
-//Screen_HX8353E myScreen;
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -27,97 +23,130 @@
 #include "utils/uartstdio.h"
 #include "inc/tm4c129encpdt.h"
 
-// Define variables and constants
 #define joystickX 2
 #define joystickY 26
 uint16_t x, y, x00, y00;
-uint32_t z;
-
+float joystickCompoundValue;
+float inputNumber;
 float pwm_word;
 uint32_t systemClock;
-// Add setup code
+//uint32_t ui32Len = 6;
+//char pcBuf;
+//char menuMessage[] =
+//        "Insert light percentage values between 0 and 100 (NaN -> 0.0, input>100 -> 100.0)\n";
+
 void setup()
 {
     x00 = 0;
     y00 = 0;
-//    pinMode(ledGreen, OUTPUT);
-//    pinMode(ledBlue, OUTPUT);
-//    pinMode(ledRed, OUTPUT);
-
-    // Set the clock frequency
-
-    systemClock = SysCtlClockFreqSet(
-            (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL
-                    | SYSCTL_CFG_VCO_480),
-            16000);
-
-    pwm_word = systemClock / 200;
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable the pins of GPIOF
-    SysCtlPWMClockSet(SYSCTL_PWMDIV_1); // Set the PWM clock config. (setting the rate in comparasion with the processor clock)
-    // Reset the PWM
-    SysCtlPeripheralDisable(SYSCTL_PERIPH_PWM0);
-    SysCtlPeripheralReset(SYSCTL_PERIPH_PWM0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
-
-    GPIOPinConfigure (GPIO_PF2_M0PWM2); // Configure pin F2
-    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2); // Configure pins for use by UART peripheral
-
-    PWMGenConfigure(
-            PWM0_BASE, PWM_GEN_1,
-            PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC | PWM_GEN_MODE_DBG_RUN);
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, pwm_word);
-    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, pwm_word / 100); // Set the PWM generator pulse width
-    PWMGenEnable(PWM0_BASE, PWM_GEN_1); // Enable the PWM generator
-    PWMOutputState(PWM0_BASE, PWM_OUT_2_BIT, true); // Enable the output of PWM0
+    inputNumber = 0.0f;
+    ConfigureUART();
+    ConfigurePWM(&pwm_word);
 }
 
 // Add loop code
 void loop()
 {
-//    analogWrite(ledRed, 254);
     x = map(analogRead(joystickX), 0, 4096, 0, 128);
     y = map(analogRead(joystickY), 0, 4096, 128, 0);
-    if (x < 1)
-        x = 1;
-    if (x > 126)
-        x = 126;
-    if (y < 1)
-        y = 1;
-    if (y > 126)
-        y = 126;
 
-//    digitalWrite(ledBlue, HIGH);
-//    digitalWrite(ledGreen, HIGH);
+    x = x < 1 ? 1 : x;
+    x = x > 126 ? 126 : x;
+    y = y < 1 ? 1 : y;
+    y = y > 126 ? 126 : y;
+
     if ((x00 != x) || (y00 != y))
     {
-        z = (uint32_t) ((x - 64) * (x - 64) + (y - 64) * (y - 64)) >> 8;
+        joystickCompoundValue = ((x - 64) * (x - 64) + (y - 64) * (y - 64))
+                >> 5;
 
-//        digitalWrite(ledGreen, HIGH);
     }
 
-    if (z <= 0)
+    if (joystickCompoundValue >= 1)
     {
-        z = 0.0f;
+        inputNumber = joystickCompoundValue;
     }
-    else if (z >= 90)
-    {
-        z = 100.0f;
-    }else{
-        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, (pwm_word / 10) * z); // Set the PWM generator pulse width
-    }
-
-
-
-//        if (z > 4)      colour = redColour;
-//        else if (z > 1) colour = yellowColour;
-//        else            colour = greenColour;
+//    else
+//    {
+//        UARTprintf(menuMessage);
+//        UARTgets(&pcBuf, ui32Len);
+//        inputNumber = strtof(&pcBuf, NULL);
 //
-//        myScreen.dRectangle(x00-1, y00-1, 3, 3, blackColour);
-//        myScreen.dRectangle(x-1, y-1, 3, 3, colour);
-//        x00 = x;
-//        y00 = y;
+//    }
 
-//    myScreen.gText(0, myScreen.screenSizeY()-myScreen.fontSizeY(),
-//                   "x=" + i32toa((int16_t)x-64, 10, 1, 6) +" y=" + i32toa(64-(int16_t)y, 10, 1, 6),
-//                   colour);
+    if (inputNumber <= 0 || inputNumber >= 100)
+    {
+        inputNumber = (inputNumber <= 0 ? 0.0f : 100.0f);
+
+        //Disable PWM and enable digital output
+        PWMOutputState(PWM0_BASE, PWM_OUT_3_BIT, false);
+        GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,
+                     (inputNumber <= 0 ? 0 : GPIO_PIN_3));
+
+    }
+    else
+    {
+        //Enable PWM mode again
+        GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+        GPIOPinConfigure (GPIO_PF3_M0PWM3);
+        PWMOutputState(PWM0_BASE, PWM_OUT_3_BIT, true);
+
+        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3,
+                         (pwm_word / 100) * (inputNumber / 3)); //Pulse width is defined as the number of PWM clock ticks
+    }
+
+    UARTprintf("=> %d \n", (int) inputNumber);
+
 }
+
+void ConfigureUART(void)
+{
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))
+    {
+    }
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinConfigure (GPIO_PA0_U0RX);
+    GPIOPinConfigure (GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1); // Configure pins for use by UART peripheral
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC); // Set the baud clock source for the specified UART
+    UARTStdioConfig(0, 115200, 16000000); // Configure the UART console
+}
+
+void ConfigurePWM(float *pwm_word)
+{
+    uint32_t systemClock;
+
+    // This function configures the main system clocking for the device. ...FreqSet(required configuration, requested processor frequency)
+    systemClock = SysCtlClockFreqSet(
+            (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL
+                    | SYSCTL_CFG_VCO_480),
+            16000);
+//    UARTprintf("%d\n", systemClock); //Aprox 10000000
+    *pwm_word = systemClock / 200;
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable GPIO F pins
+
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_1); //Configures the rate of the clock provided to the PWM module as a ratio of the processor clock
+
+    SysCtlPeripheralDisable(SYSCTL_PERIPH_PWM0);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_PWM0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+
+    //Configures pin(s) for use by the PWM peripheral.  ...PWM(address of the GPIO port, bit-packed representation of the pin/s)
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+    GPIOPinConfigure (GPIO_PF3_M0PWM3);
+
+    PWMGenConfigure(
+            PWM0_BASE, PWM_GEN_1,
+            PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC | PWM_GEN_MODE_DBG_RUN);
+
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, *pwm_word); // Period of the generator block is defined as the number of PWM clock ticks between pulses on the generator
+
+    PWMGenEnable(PWM0_BASE, PWM_GEN_1);
+
+    PWMOutputState(PWM0_BASE, PWM_OUT_3_BIT, false);
+
+}
+
