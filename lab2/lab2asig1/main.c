466bc267a828c10fb984f0777ff7cabc47f3f7f9
sescobar99 <santi.e.c@hotmail.com>
2022-09-28 20:15:37 +0200

    lab2 assignment1 0 + 100 cases working

--- lab2/lab2asig1/main.c
+++ lab2/lab2asig1/main.c
@@ -1,3 +1,14 @@
+/**
+ * MDU ES Lab2 Assignment1
+ * Write a program which lets the user decide on the brightness of a LED using the PWM library  for  the  embedded  system.
+ * The  expressed  values  should  be  on  a  percentage  basis (between 0% and 100%), and should represent the LED intensity.
+ * The program should be able to set any percentage of light.
+ * The program must handle 3 different cases. Two special cases of 0% and 100% should turn off and on the LED using UART (functions for LEDs used in lab 1), respectively.
+ * Other values more than 0% and less than 100% should be expressed using PWM library Button input.
+ *  Josep Naranjo
+ *  &
+ *  Santiago Escobar
+ */
 //*****************************************************************************
 #include <stdbool.h>
 #include <stdint.h>
@@ -32,84 +43,89 @@ void ConfigureUART(void)
     UARTStdioConfig(0, 115200, 16000000); // Configure the UART console
 }
 
-/**
- * main.c
- */
-int main(void)
+void ConfigurePWM(float *pwm_word)
 {
-    ConfigureUART();
-
-    uint32_t ui32Len = 6;
-    char pcBuf;
-    float inputNumber = 0.0f;
-
-    // Set the clock frequency
-    float pwm_word;
     uint32_t systemClock;
+
+    // This function configures the main system clocking for the device. ...FreqSet(required configuration, requested processor frequency)
     systemClock = SysCtlClockFreqSet(
             (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL
                     | SYSCTL_CFG_VCO_480),
             16000);
+//    UARTprintf("%d\n", systemClock); //Aprox 10000000
+    *pwm_word = systemClock / 200;
 
-    pwm_word = systemClock / 200;
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable the pins of GPIOF
-    SysCtlPWMClockSet(SYSCTL_PWMDIV_1); // Set the PWM clock config. (setting the rate in comparasion with the processor clock)
-    // Reset the PWM
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable GPIO F pins
+
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_1); //Configures the rate of the clock provided to the PWM module as a ratio of the processor clock
+
     SysCtlPeripheralDisable(SYSCTL_PERIPH_PWM0);
     SysCtlPeripheralReset(SYSCTL_PERIPH_PWM0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
 
-    GPIOPinConfigure(GPIO_PF2_M0PWM2); // Configure pin F2
-    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2); // Configure pins for use by UART peripheral
+    //Configures pin(s) for use by the PWM peripheral.  ...PWM(address of the GPIO port, bit-packed representation of the pin/s)
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+    GPIOPinConfigure(GPIO_PF3_M0PWM3);
 
     PWMGenConfigure(
             PWM0_BASE, PWM_GEN_1,
-            PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC | PWM_GEN_MODE_DBG_RUN); // Configure the PWM generator
-    // PWM_GEN_MODE_DOWN: generates a left-aligned signal
-    // PWM_GEN_MODE_NO_SYNC: set a asynch gen.
-    // PWM_GEN_MODE_DBG_RUN: stills runs even is the processor is not
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, pwm_word); // Set the PWM generator clock period
-//    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, pwm_word / 100); // Set the PWM generator pulse width
-    PWMGenEnable(PWM0_BASE, PWM_GEN_1); // Enable the PWM generator
-    PWMOutputState(PWM0_BASE, PWM_OUT_2_BIT, true); // Enable the output of PWM0
+            PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC | PWM_GEN_MODE_DBG_RUN);
+
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, *pwm_word); // Period of the generator block is defined as the number of PWM clock ticks between pulses on the generator
+
+    PWMGenEnable(PWM0_BASE, PWM_GEN_1);
+
+    PWMOutputState(PWM0_BASE, PWM_OUT_3_BIT, false);
+
+}
+
+//***********************************************************************
+// main.c
+//***********************************************************************
+int main(void)
+{
+
+    uint32_t ui32Len = 6;
+    char pcBuf;
+    float inputNumber = 0.0f;
+    char menuMessage[] =
+            "Insert light percentage values between 0 and 100 (NaN -> 0.0, input>100 -> 100.0)\n";
+    float pwm_word;
+
+    ConfigureUART();
+    ConfigurePWM(&pwm_word);
 
     while (1)
     {
-//        while (!UARTCharsAvail(UART0_BASE))       {       }
-//        UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
-//         (UART0_BASE)
-
-        UARTprintf(
-                "Insert light percentage values between 0 and 100 (NaN recognized as 0 , >100 values as 100\n");
+        UARTprintf(menuMessage);
         UARTgets(&pcBuf, ui32Len);
-        inputNumber = strtof(&pcBuf, NULL); //&end
+        inputNumber = strtof(&pcBuf, NULL);
 
-        if (inputNumber <= 0)
+        if (inputNumber <= 0 || inputNumber >= 100)
         {
-            inputNumber = 0.0f;
-            LEDWrite(PWM_OUT_2, 0);
+            inputNumber = (inputNumber <= 0 ? 0.0f : 100.0f);
+
+            //Disable PWM and enable digital output
+            PWMOutputState(PWM0_BASE, PWM_OUT_3_BIT, false);
+            GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,
+                         (inputNumber <= 0 ? 0 : GPIO_PIN_3));
+
         }
-        else if (inputNumber >= 100)
+        else
         {
-            inputNumber = 100.0f;
-        }else{
-            PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, (pwm_word / 100) * inputNumber); // Set the PWM generator pulse width
+            //Enable PWM mode again
+            GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+            GPIOPinConfigure(GPIO_PF3_M0PWM3);
+            PWMOutputState(PWM0_BASE, PWM_OUT_3_BIT, true);
 
+            PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3,
+                             (pwm_word / 100) * (inputNumber / 3)); //Pulse width is defined as the number of PWM clock ticks
         }
 
-
-        UARTprintf("%d \n", (int) inputNumber);
+        UARTprintf("=> %d \n", (int) inputNumber);
 
     }
-
-    /*
-
-     UARTConfigSetExpClk(
-     UART0_BASE, SysCtlClockGet(), 38400,
-     (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-     */
-
-    return 0;
 }
 
 //*****************************************************************************
@@ -123,5 +139,8 @@ int main(void)
 //        UARTgetc();
 //        UARTvprintf(*pcString, vaArgP);
 //        UARTprintf(*pcString, *pcString2);
-//********
+//********uart usage
+//        while (!UARTCharsAvail(UART0_BASE))       {       }
+//        UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
+//         (UART0_BASE)
 
