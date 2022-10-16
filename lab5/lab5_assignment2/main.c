00bd250b08eff0b16851494f9797af7e9ca8794a
joseptnl <joseptnl@gmail.com>
2022-10-16 16:48:44 +0200

    Lab 5

--- lab5/lab5_assignment2/main.c
+++ lab5/lab5_assignment2/main.c
@@ -2,6 +2,9 @@
 #include "task.h"
 #include "queue.h"
 #include "croutine.h"
+#include "semphr.h"
+
+#define SECOND 1500000
 
 //*******************************************************
 // Debug (UART)
@@ -17,6 +20,8 @@
 #include "driverlib/uart.h"
 #include "inc/tm4c129encpdt.h"
 
+static volatile SemaphoreHandle_t bin_sem;
+
 void configureUART(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -30,12 +35,13 @@ void configureUART(void)
                         UART_CONFIG_PAR_NONE));
 }
 
-void printString (char* str)
+void printString(char* str)
 {
     while (*str)
     {
         UARTCharPut(UART0_BASE, *(str++));
     }
+    UARTCharPut(UART0_BASE, 13);
 }
 
 //*******************************************************
@@ -45,33 +51,54 @@ void printString (char* str)
 // High priority task
 void vTaskA(void* pvParameters)
 {
-    printString("   Task A: Started    \n");
-    printString("   Task A: Finished   \n");
+    int i;
+    vTaskDelay(400 / portTICK_PERIOD_MS);
+    printString("Task A: Started\n");
+    for (i = 0; i < SECOND * 2; i++)
+    {
+    }
+    // Enters cs
+    xSemaphoreTake(bin_sem, 0);
+    printString("Task A: sem take\n");
+    for (i = 0; i < SECOND * 5; i++)
+    {
+    }
+    printString("Task A: sem give\n");
+    xSemaphoreGive(bin_sem);
+    // Leaves cs
+    printString("Task A: Finished\n");
     vTaskDelete(NULL);
 }
 // Medium priority task
 void vTaskB(void* pvParameters)
 {
-    printString("   Task B: Started    \n");
-    printString("   Task B: Finished   \n");
+    int i;
+    printString("Task B: Started\n");
+
+    printString("Task B: Finished\n");
     vTaskDelete(NULL);
 }
 // Low priority task
 void vTaskC(void* pvParameters)
 {
-    printString("   Task C: Started    \n");
-    // Waits 200 ms
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    // Enters critical section
-    //taskENTER_CRITICAL();
-    printString("   Task C: sem take    \n");
-    // Waits 700 ms
-    vTaskDelay(700 / portTICK_PERIOD_MS);
-    printString("   Task C: sem give    \n");
-    //taskEXIT_CRITICAL();
-    // Waits 200 ms
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    printString("   Task C: Finished   \n");
+    int i;
+    printString("Task C: Started\n");
+    for (i = 0; i < SECOND * 2; i++)
+    {
+    }
+    // Enters cs
+    xSemaphoreTake(bin_sem, 0);
+    printString("Task C: sem take\n");
+    for (i = 0; i < SECOND * 7; i++)
+    {
+    }
+    printString("Task C: sem give\n");
+    xSemaphoreGive(bin_sem);
+    // Leaves cs
+    for (i = 0; i < SECOND * 2; i++)
+    {
+    }
+    printString("Task C: Finished\n");
     vTaskDelete(NULL);
 }
 
@@ -81,15 +108,20 @@ void vTaskC(void* pvParameters)
 
 void vScheduling()
 {
+    // Create semaphore
+    bin_sem = xSemaphoreCreateBinary();
+
+    // Create tasks
     static unsigned char ucParameterToPass1, ucParameterToPass2,
             ucParameterToPass3;
-    xTaskHandle xHandle1, xHandle2, xHandle3;    // Create tasks
+    xTaskHandle xHandle1, xHandle2, xHandle3;
     xTaskCreate(vTaskA, "TASKA", configMINIMAL_STACK_SIZE, &ucParameterToPass1,
                 tskIDLE_PRIORITY + 3, &xHandle1);
-    xTaskCreate(vTaskB, "TASKB", configMINIMAL_STACK_SIZE, &ucParameterToPass2,
-                tskIDLE_PRIORITY + 2, &xHandle2);
+//    xTaskCreate(vTaskB, "TASKB", configMINIMAL_STACK_SIZE, &ucParameterToPass2,
+//                tskIDLE_PRIORITY + 2, &xHandle2);
     xTaskCreate(vTaskC, "TASKC", configMINIMAL_STACK_SIZE, &ucParameterToPass3,
                 tskIDLE_PRIORITY + 1, &xHandle3);
+
     // Start scheduler
     vTaskStartScheduler();
 }
