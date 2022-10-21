9d3ea6b1b49e5765565204845a9140a69b46b406
sescobar99 <santi.e.c@hotmail.com>
2022-10-21 14:35:35 +0200

    lab5 adddifferent workloads + comment

--- lab5/lab5_assignment2/main.c
+++ lab5/lab5_assignment2/main.c
@@ -1,12 +1,28 @@
+/**
+ * MDU ES Lab5 Assignment 2+3
+ * 2.Implement three different tasks that have periods, release times and priorities in such a manner that the priority inversion takes place.
+ * If done properly, the execution of a correctly implemented priority inversion will result in a deadline miss of the tasks.
+ * To visualize the execution of the different tasks, you are  required to output certain keypoints to the UART.
+ *
+ * 3.Write a software function to detect the deadline miss.
+ * Write the following information via the UART: which task misses its deadline and by how much time the deadline is missed.
+ * All tasks should have this deadlines miss detection.
+ *  Josep Naranjo
+ *  &
+ *  Santiago Escobar
+ */
+//*****************************************************************************
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 #include "croutine.h"
 #include "semphr.h"
 
-
-#define DEBUG 1
-#define TIME_SIZE 1000
+#define PRINT_WORKLOAD 1
+#define TIME_SIZE 1000 //1000 -> 1 time unit = 1 second
+#define TASK_A_MISS_DEADLINE 1
+#define TASK_B_MISS_DEADLINE 0
+#define TASK_C_MISS_DEADLINE 0
 
 //*******************************************************
 // Debug (UART)
@@ -25,9 +41,35 @@
 
 static volatile SemaphoreHandle_t bin_sem;
 
+
+//*******************************************************
+// Tasks code
+//*******************************************************
+#if TASK_A_MISS_DEADLINE
 static const unsigned int task_A_period = pdMS_TO_TICKS(10 * TIME_SIZE);
 static const unsigned int task_B_period = pdMS_TO_TICKS(20 * TIME_SIZE);
 static const unsigned int task_C_period = pdMS_TO_TICKS(20 * TIME_SIZE);
+char *msg ="'A' will miss it´s deadline, because of the priority inversion\n";
+#endif
+
+#if TASK_B_MISS_DEADLINE
+static const unsigned int task_A_period = pdMS_TO_TICKS(10 * TIME_SIZE);
+static const unsigned int task_B_period = pdMS_TO_TICKS(2 * TIME_SIZE);
+static const unsigned int task_C_period = pdMS_TO_TICKS(20 * TIME_SIZE);
+char *msg = "'B' will miss it´s deadline, C will starve\n";
+#endif
+
+#if TASK_C_MISS_DEADLINE
+static const unsigned int task_A_period = pdMS_TO_TICKS(10 * TIME_SIZE);
+static const unsigned int task_B_period = pdMS_TO_TICKS(18 * TIME_SIZE);
+static const unsigned int task_C_period = pdMS_TO_TICKS(2 * TIME_SIZE);
+char *msg ="'A' & 'C' will miss deadlines\n";
+#endif
+
+
+//*******************************************************
+// Helper functions
+//*******************************************************
 
 void configureUART(void)
 {
@@ -51,19 +93,23 @@ void printString(char* str)
     UARTCharPut(UART0_BASE, 13);
 }
 
+
+//Simulates work for the tasks
 void idleWork(int seconds, char* pcTask)
 {
     TickType_t currentTick = xTaskGetTickCount();
-#if DEBUG
+#if PRINT_WORKLOAD
     char str[50];
-    sprintf(str, "Task %s: WORKLOAD", pcTask);
-    int i = 0;
     printString("\n");
+    sprintf(str, "\033[ATask %s: workload\n", pcTask);
+    int i = 0;
 #endif
-    while (xTaskGetTickCount() - currentTick < pdMS_TO_TICKS(seconds * TIME_SIZE))
+
+    while (xTaskGetTickCount() - currentTick
+            < pdMS_TO_TICKS(seconds * TIME_SIZE))
     {
-#if DEBUG
-        if (i % 5000 * TIME_SIZE == 0)
+#if PRINT_WORKLOAD
+        if (i % 10000 * TIME_SIZE == 0)
         {
             printString(str);
         }
@@ -72,19 +118,19 @@ void idleWork(int seconds, char* pcTask)
     }
 }
 
+// Compares the time the task is executed against the time of its deadline
 void deadlineMiss(TickType_t xLastWakeTime, TickType_t period, char* pcTask)
 {
     TickType_t xPeriodLimit = xLastWakeTime + period;
     TickType_t xActualTime = xTaskGetTickCount();
+
+    //Checks deadline. If task missed it shows information otherwise delays until next period
     if (xPeriodLimit < xActualTime)
     {
-        char str[50];
-
         unsigned int ms_miss = (xActualTime - xPeriodLimit) * portTICK_PERIOD_MS;
-
+        char str[50];
         sprintf(str, "Task %s: DEADLINE MISSED by %u ms \n", pcTask, ms_miss);
         printString(str);
-
     }
     else
     {
@@ -97,10 +143,15 @@ void deadlineMiss(TickType_t xLastWakeTime, TickType_t period, char* pcTask)
 // Tasks code
 //*******************************************************
 
-// High priority task
+/*
+ * High priority task
+ * Workload: 4 time units (2 are inside the critical section)
+ * Works 1 time unit->Takes Semaphore-> Works 2 time units-> Gives Semaphore -> Works 1 time units-> Checks if deadline was missed
+ * Starts after an initial delay of 3 time units
+ */
 void vTaskA(void* pvParameters)
 {
-    TickType_t xLastWakeTime, xPeriodLimit, xActualTime;
+    TickType_t xLastWakeTime;
     vTaskDelay(pdMS_TO_TICKS(3 * TIME_SIZE));
     while (1)
     {
@@ -110,13 +161,13 @@ void vTaskA(void* pvParameters)
         idleWork(1, "A");
         // Enters cs
         xSemaphoreTake(bin_sem, portMAX_DELAY);
-        printString("Task A: sem take\n");
+        printString("Task A: sem TAKE\n");
         int i;
         for (i = 0; i < 2; i++)
         {
             idleWork(1, "A");
         }
-        printString("Task A: sem give\n");
+        printString("Task A: sem GIVE\n");
         xSemaphoreGive(bin_sem);
         // Leaves cs
         idleWork(1, "A");
@@ -125,10 +176,16 @@ void vTaskA(void* pvParameters)
         deadlineMiss(xLastWakeTime, task_A_period, "A");
     }
 }
-// Medium priority task
+
+/*
+ * Medium priority task
+ * Workload: 4 time units
+ * Works 1 time unit->Takes Semaphore-> Works 2 time units-> Gives Semaphore -> Works 1 time units-> Checks if deadline was missed
+ * Starts after an initial delay of 6 time units
+ */
 void vTaskB(void* pvParameters)
 {
-    TickType_t xLastWakeTime, xPeriodLimit, xActualTime;
+    TickType_t xLastWakeTime;
     vTaskDelay(pdMS_TO_TICKS(6 * TIME_SIZE));
     while (1)
     {
@@ -145,10 +202,16 @@ void vTaskB(void* pvParameters)
         deadlineMiss(xLastWakeTime, task_B_period, "B");
     }
 }
-// Low priority task
+
+/*
+ * Low priority task
+ * Workload: 4 time units
+ * Works 1 time unit->Takes Semaphore-> Works 6 time units-> Gives Semaphore -> Works 1 time units-> Checks if deadline was missed
+ * Starts after an initial delay of 0 time units
+ */
 void vTaskC(void* pvParameters)
 {
-    TickType_t xLastWakeTime, xPeriodLimit, xActualTime;
+    TickType_t xLastWakeTime;
     while (1)
     {
         xLastWakeTime = xTaskGetTickCount();
@@ -157,13 +220,13 @@ void vTaskC(void* pvParameters)
         idleWork(1, "C");
         // Enters cs
         xSemaphoreTake(bin_sem, portMAX_DELAY);
-        printString("Task C: sem take\n");
+        printString("Task C: sem TAKE\n");
         int i;
         for (i = 0; i < 6; i++)
         {
             idleWork(1, "C");
         }
-        printString("Task C: sem give\n");
+        printString("Task C: sem GIVE\n");
         xSemaphoreGive(bin_sem);
         // Leaves cs
         idleWork(1, "C");
@@ -204,6 +267,7 @@ void vScheduling()
 int main(void)
 {
     configureUART();
+    printString(msg);
     vScheduling();
 
     while (1)
